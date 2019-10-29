37f7f7e3902e256484bf044db2c9fd139ddddfc4
Karlis Cars <karlis.cars@gmail.com>
2019-10-29 15:20:28 +0200

    Started makeing

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var farhenheitLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func tempSlider(_ sender: Any) {
+    }
+    
 
 }
 
